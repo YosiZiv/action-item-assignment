292d5c044bcde2ecb81ff9e5c09cd0e3e7a39151
yosi ziv <yosizivjs@gmail.com>
2021-01-15 20:34:35 +0200

    custom errors handler

---
+++ src/errors/not-found-error.ts
@@ -0,0 +1,10 @@
+import { CustomError } from "./custom-error";
+
+export class NotFoundError extends CustomError {
+  constructor() {
+    super("Route not found");
+    Object.setPrototypeOf(this, NotFoundError.prototype);
+  }
+  statusCode = 404;
+  serializeErrors = () => [{ message: "Not Found" }];
+}
